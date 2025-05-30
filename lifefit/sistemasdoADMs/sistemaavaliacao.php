0da36b979d0083f9eb09b8be2a6921298cf98387
Marcussousasousa <MARCUSGAMEMCPE@GMAIL.COM>
2025-05-30 18:50:36 -0300

    sistemas

---
+++ lifefit/sistemasdoADMs/sistemaavaliacao.php
@@ -0,0 +1,36 @@
+
+<!--php aqui minuto (0:48)-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema - GN</title>
+    <link rel="stylesheet" href="../style/sistema.css">
+</head>
+<body>
+
+
+    <div class="m-5">
+        <table class="table text-white table-bg">
+        <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Data-Avaliação</th>
+                    <th scope="col">Plano de Exercícios</th>
+                    <th scope="col">Plano Alimentar</th>
+                     <th scope="col">...</th>
+                </tr>
+        </thead>
+
+        <tbody>
+         <!-- luis aqui é php no video (4:00) -->
+        </tbody>
+</table>
+    </div>
+    
+</body>
+</html>
